99eb672b2909dfaeecaba869f79bffdec04acf0b
pakashiva <pakashivaprasad2004@gmail.com>
2026-07-02 14:20:05 +0530

    Implementing CRD

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
 
 app = Flask(__name__)
 
+db = [] 
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", notes = db)
 
-@app.route("/about")
-def about_page():
-    return "We are builders."
 
+@app.route("/add" , methods = ['POST'])
+def add_notes():
+    title = request.form['title']
+    content = request.form['content']
+
+    note = {
+        "id": len(db),
+        "tilte": title,
+        "content": content
+    }
+
+    db.append(note)
+
+    return redirect("/")
+
+@app.route("/delete/<int:index>" , methods = ['GET' , 'POST'])
+def delete_note(index):
+    if 0 <= index <= len(db):
+        db.pop(index)
+
+    return redirect("/")
 
 if __name__ == "__main__": 
     app.run()
